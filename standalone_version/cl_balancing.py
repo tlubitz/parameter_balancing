25175ce6a985810dd29b933255ed510f48c35e34
Timo Lubitz <timo.lubitz@gmail.com>
2018-10-12 14:11:52 +0200

    minor layout updates; mainly introducing the improved commandline interface which also enables package embedding

---
+++ standalone_version/cl_balancing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import argparse
+
+try: from . import parameter_balancing
+except: import parameter_balancing
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('sbml', help='Path to an SBML file.')
+    parser.add_argument('--sbtab_data', help='Path to an SBtab data file.')
+    parser.add_argument('--sbtab_prior', help='Path to an SBtab prior file.')
+    parser.add_argument('--sbtab_options', help='Path to an SBtab options file.')
+    parser.add_argument('--output_name', help='Choose a name for the output files.')
+    parser.add_argument('-l', '--pb_log', help='Flag to print a log file.', action='store_true')
+    parser.add_argument('-p', '--no_pseudo_values', help='Flag for disabling the usage of pseudo values.', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Flag to display script messages.', action='store_true')
+
+    args = parser.parse_args()
+
+    parameter_balancing.parameter_balancing_wrapper(args.sbml,
+                                                    args.sbtab_data,
+                                                    args.sbtab_prior,
+                                                    args.sbtab_options,
+                                                    args.verbose,
+                                                    args.no_pseudo_values,
+                                                    args.output_name,
+                                                    args.pb_log)
+
